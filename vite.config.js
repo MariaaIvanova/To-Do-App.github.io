62facf4b692c09b37aef7ec11ce5d80b58341772
Maria Ivanova <mariya.kl.ivanova@gmail.com>
2025-07-08 12:04:33 +0200

    Ready

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], base: 'To-Do-App.github.io/',  
   server: {
     port: 3000,
     open: true
